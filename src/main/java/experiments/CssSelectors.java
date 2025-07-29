be9c5c0dea687c0d95eaca19c81422a8a9b336e5
MedSveta <sveta1978medved@gmail.com>
2025-07-29 19:18:11 +0300

    Css selectors

--- src/main/java/experiments/CssSelectors.java
+++ src/main/java/experiments/CssSelectors.java
@@ -1,6 +1,7 @@
 package experiments;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -22,8 +23,36 @@ public class CssSelectors {
                 .cssSelector("img[src='/images/Toolsqa.jpg']"));
         System.out.println("TagName element --> " + imgTools.getTagName());
         System.out.println("Attribute element -->" + imgTools.getAttribute("src"));
+        pause(4000);
+
+        //WebElement divElements = driver.findElement(By.cssSelector("div[class='card-up']"));
+        WebElement divElements = driver.findElement(By.cssSelector(".card-up"));
+        divElements.click();
+        WebElement btnRadioButton = driver.findElement(By.cssSelector("#item-2"));
+        //WebElement btnRadioButton = driver.findElement(By.id("item-2"));
+        btnRadioButton.click();
+        //WebElement radioButtonYes = driver.findElement(By.id("yesRadio"));
+        //WebElement radioButtonYes = driver.findElement(By.cssSelector("label[for='yesRadio']"));
+        WebElement radioButtonYes1 = driver.findElement(By.cssSelector("*[for='yesRadio']"));
+        WebElement radioButtonYes = driver.findElement(By.xpath("//label[@for='yesRadio']"));
+        radioButtonYes.click();
+        pause(4000);
+        driver.navigate().back();
+        driver.navigate().back();
+        hideBanner();
+        hideFooter();
+        WebElement divBookStore = driver.findElement(By
+                .cssSelector("div[class='category-cards'] div[class='card mt-4 top-card']:last-child"));
+        divBookStore.click();
+        pause(4000);
+        driver.navigate().back();
+
+        WebElement divWidgets = driver.findElement(By.cssSelector("div[class='category-cards']" +
+                " div[class='card mt-4 top-card']:nth-child(4) div[class='card-body'] h5"));
+        System.out.println(divWidgets.getText());
 
         pause(4000);
+
         driver.quit();
         //driver.close();
     }
@@ -34,5 +63,14 @@ public class CssSelectors {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void hideBanner(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display='none'");
+    }
+    public void hideFooter(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('footer').style.display='none'");
     }
 }
